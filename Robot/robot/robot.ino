1e3e9bb02af4daf16a26d486c49196320a3ef42a
bassza009 <bsspawat@gmail.com>
2026-07-25 17:57:24 +0700

    add moreee

--- Robot/robot/robot.ino
+++ Robot/robot/robot.ino
@@ -1,3 +1,9 @@
+#include "Arduino_LED_Matrix.h"
+#include "/home/keaw/github/IOT-Robot/Robot/animation/animation.h"
+#include "Servo.h"
+
+Servo servo;
+ArduinoLEDMatrix matrix;
 
 /*
   analog(0)
@@ -6,7 +12,7 @@
   analog(3071) 3.75 V
   analog(4095) 5 V
   */
- 
+
 //Left motor
 int ENA = 10;
 int IN1 = 9;
@@ -29,146 +35,238 @@ void setup() {
   //analogWriteResolution(12);
   // put your setup code here, to run once:
   Serial.begin(9600);
-//Left motor set up
-  pinMode(ENA,OUTPUT);
-  pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
+  matrix.begin();
+  //Left motor set up
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
   //Right motor set up
-  pinMode(ENB,OUTPUT);
-  pinMode(IN3,OUTPUT);
-  pinMode(IN4,OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
   //Servo
-   //pinMode(SV,OUTPUT);
-
+  servo.attach(SV);
 }
 
-void increse_speed(){
-  digitalWrite(IN1,0);
-  digitalWrite(IN2,1);
-  digitalWrite(IN3,0);
-  digitalWrite(IN4,1);
+void increse_speed() {
+  digitalWrite(IN1, 0);
+  digitalWrite(IN2, 1);
+  digitalWrite(IN3, 0);
+  digitalWrite(IN4, 1);
 
-  for(int i = 2050;i<4096;i++){
-    analogWrite(ENA,i-B_L);
-    analogWrite(ENB,i-B_R);
+  for (int i = 2050; i < 4096; i++) {
+    analogWrite(ENA, i - B_L);
+    analogWrite(ENB, i - B_R);
   }
-  for(int i = 4095;i>2051;i--){
-    analogWrite(ENA,i-B_L);
-    analogWrite(ENB,i-B_R);
+  for (int i = 4095; i > 2051; i--) {
+    analogWrite(ENA, i - B_L);
+    analogWrite(ENB, i - B_R);
   }
 }
 
-void  forward(int speed_motorL = 200,int speed_motorR = 200,int B_L = 0,int B_R = 0){
+void forward(int speed_motorL = 200, int speed_motorR = 200, int B_L = 0, int B_R = 15) {
   // Define parameter for L298N
+
+
+  digitalWrite(IN1, 0);
+  digitalWrite(IN2, 1);
+
+  digitalWrite(IN3, 0);
+  digitalWrite(IN4, 1);
+
+
+  //left motor
+  analogWrite(ENA, speed_motorL - B_R);
+  //right motor
+  analogWrite(ENB, speed_motorR - B_L);
+  //animation
+  int total_frames = sizeof(walk) / sizeof(walk[0]);
+  for (int i = 0; i < total_frames; i++) {
+    loadFlippedYFrame(walk[i]);
+    delay(walk[i][3]); // 66 ms frame delay
+  }
+}
+
+void backward(int speed_motorL = 200, int speed_motorR = 200, int B_L = 0, int B_R =15) {
+  // Define parameter for L298N
+  digitalWrite(IN1, 1);
+  digitalWrite(IN2, 0);
+
+  digitalWrite(IN3, 1);
+  digitalWrite(IN4, 0);
+
+  //left motor
+  analogWrite(ENA, speed_motorL - B_R);
+  //right motor
+  analogWrite(ENB, speed_motorR - B_L);
+
+  int total_frames = sizeof(walk) / sizeof(walk[0]);
+  for (int i = 0; i < total_frames; i++) {
+    loadFlippedXFrame(walk[i]);
+    delay(walk[i][3]); // 66 ms frame delay
+  }
+}
+
+
+
+void right_turn(int speed_motorR = 200,int speed_motorL = 0, int B_L = 0, int B_R = 0) {
+
+
+  digitalWrite(IN3, 0);
+  digitalWrite(IN4, 1);
+
+  //Define speed for motor
+ //left motor
+  analogWrite(ENA, speed_motorL - B_R);
+  //right motor
+  analogWrite(ENB, speed_motorR - B_L);
+  Serial.print("Right turn");
+  int total_frames = sizeof(side_walk) / sizeof(side_walk[0]);
+  for (int i = 0; i < total_frames; i++) {
+    loadFlippedYFrame(side_walk[i]);
+    delay(side_walk[i][3]); // 66 ms frame delay
+  }
+}
+
+
+
+
+
+void left_turn(int speed_motorL = 200, int speed_motorR = 0,int B_L = 0, int B_R = 0) {
+
+  // Define parameter for L298N
+
+  digitalWrite(IN1, 0);
+  digitalWrite(IN2, 1);
+
+  //Define speed for motor
+
+  //left motor
+  analogWrite(ENA, speed_motorL - B_R);
+  //right motor
+  analogWrite(ENB, speed_motorR - B_L);
+  Serial.println("Left turn");
+
  
-  
-  digitalWrite(IN1,0);
-  digitalWrite(IN2,1);
-  
-  digitalWrite(IN3,0);
-  digitalWrite(IN4,1);
-
-
-  //left motor
-  analogWrite(ENA,speed_motorL-B_R);
-  //right motor
-  analogWrite(ENB,speed_motorR-B_L);
-  
+  // Redo forward order with swapped X and Y axes
+  int total_frames = sizeof(side_walk) / sizeof(side_walk[0]);
+  for (int i = 0; i < total_frames; i++) {
+    loadFlippedXYFrame(side_walk[i]);
+    delay(side_walk[i][3]); // 66 ms frame delay
+  }
 }
 
-void backward(int speed_motorL = 200,int speed_motorR = 200,int B_L = 0,int B_R = 0){
-  
-  // Define parameter for L298N 
-  digitalWrite(IN1,1);
-  digitalWrite(IN2,0);
-  
-  digitalWrite(IN3,1);
-  digitalWrite(IN4,0);
+void left_turnback(int speed_motorL = 200, int speed_motorR = 200, int B_L = 0, int B_R = 0) {
+
+  // Define parameter for L298N
+  digitalWrite(IN1, 0);
+  digitalWrite(IN2, 1);
+  digitalWrite(IN3, 1);
+  digitalWrite(IN4, 0);
 
   //Define speed for motor
   //left motor
-  analogWrite(ENA,speed_motorL-B_R);
+  analogWrite(ENA, speed_motorL - B_R);
   //right motor
-  analogWrite(ENB,speed_motorR-B_L);
- 
+  analogWrite(ENB, speed_motorR - B_L);
+  int total_frames = sizeof(radar) / sizeof(radar[0]);
+  for (int i = 0; i < total_frames; i++) {
+    loadFlippedYFrame(radar[i]);
+    delay(radar[i][3]); // 66 ms frame delay
+  }
 }
-
-void right_turn(int speed_motorR = 200,int B_L = 0,int B_R = 0,int degree = 50){
-  
-  
-  digitalWrite(IN3,0);
-  digitalWrite(IN4,1);
-
-  //Define speed for motor
-  
-  //right motor
-  analogWrite(ENB,speed_motorR-B_L);
-  delay(degree);
-
-}
-
-void left_turn(int speed_motorL = 200,int B_L = 0,int B_R = 0,int degree = 50){
-  
-  // Define parameter for L298N 
-  
-  digitalWrite(IN1,0);
-  digitalWrite(IN2,1);
-
-  //Define speed for motor
-  
-  //right motor
-  analogWrite(ENA,speed_motorL-B_R );
-  delay(degree);
-
-}
-
-void left_turnback(int speed_motorL = 200,int speed_motorR = 200,int B_L = 0,int B_R = 0){
-  
-  // Define parameter for L298N 
-  digitalWrite(IN1,0);
-  digitalWrite(IN2,1);
-  digitalWrite(IN3,1);
-  digitalWrite(IN4,0);
+void right_turnback(int speed_motorL = 200, int speed_motorR = 200, int B_L = 0, int B_R = 0) {
+  // Define parameter for L298N
+  digitalWrite(IN1, 1);
+  digitalWrite(IN2, 0);
+  digitalWrite(IN3, 0);
+  digitalWrite(IN4, 1);
 
   //Define speed for motor
   //left motor
-  analogWrite(ENA,speed_motorL-B_R);
+  analogWrite(ENA, speed_motorL - B_R);
   //right motor
-  analogWrite(ENB,speed_motorR-B_L);
-  
-
+  analogWrite(ENB, speed_motorR - B_R);
+  int total_frames = sizeof(radar) / sizeof(radar[0]);
+  for (int i = 0; i < total_frames; i++) {
+    loadFlippedXYFrame(radar[i]);
+    delay(radar[i][3]); // 66 ms frame delay
+  }
 }
-void right_turnback(int speed_motorL = 200,int speed_motorR = 200,int B_L = 0,int B_R = 0){
-  // Define parameter for L298N 
-  digitalWrite(IN1,1);
-  digitalWrite(IN2,0);
-  digitalWrite(IN3,0);
-  digitalWrite(IN4,1);
+void stop() {
+  analogWrite(ENA, 0);
 
-  //Define speed for motor
-  //left motor
-  analogWrite(ENA,speed_motorL-B_R);
-  //right motor
-  analogWrite(ENB,speed_motorR-B_R);
-  
+  analogWrite(ENB, 0);
+  int total_frames = sizeof(heart) / sizeof(heart[0]);
+  for (int i = 0; i < total_frames; i++) {
+    loadFlippedYFrame(heart[i]);
+    delay(heart[i][3]); // 66 ms frame delay
+  }
 }
-void stop(){
-  analogWrite(ENA,0);
-  
-  analogWrite(ENB,0);
+
+void Sv(int degree) {
+  servo.write(degree);
   delay(1000);
 }
+void loadTransformedFrame(const uint32_t frame[4], bool flipX, bool flipY) {
+  uint32_t transformed[4] = {0, 0, 0, frame[3]};
 
-void  servo(int degree){
-  //digitalWrite(SV, 1);
-  analogWrite(SV,degree);
+  for (int y = 0; y < 8; y++) {
+    for (int x = 0; x < 12; x++) {
+      int srcBit = y * 12 + x;
+      int srcWord = srcBit / 32;
+      int srcPos = 31 - (srcBit % 32);
+      bool isSet = (frame[srcWord] >> srcPos) & 1;
+
+      if (isSet) {
+        int new_x = flipX ? (11 - x) : x;
+        int new_y = flipY ? (7 - y) : y;
+
+        int dstBit = new_y * 12 + new_x;
+        int dstWord = dstBit / 32;
+        int dstPos = 31 - (dstBit % 32);
+        transformed[dstWord] |= (1UL << dstPos);
+      }
+    }
+  }
+  matrix.loadFrame(transformed);
 }
 
+void loadFlippedXFrame(const uint32_t frame[4]) {
+  loadTransformedFrame(frame, true, false);
+}
+
+void loadFlippedYFrame(const uint32_t frame[4]) {
+  loadTransformedFrame(frame, false, true);
+}
+
+void loadFlippedXYFrame(const uint32_t frame[4]) {
+  loadTransformedFrame(frame, true, true);
+}
 void loop() {
   // put your main code here, to run repeatedly:
-  int d_time = 1000; 
-  
+  int d_time = 1000;
+
+  Sv(180);
+  Sv(90);
+  Sv(45);
+  Sv(0);
+  Sv(90);
+  /*
   forward();
-  
+  delay(1000);
+  backward();
+  delay(1000);
+  right_turn();
+  delay(1000);
+  left_turn();
+  delay(1000);
+  left_turnback();
+  delay(1000);
+  right_turnback();
+  delay(1000);
+  stop();
+  delay(1000);
+  */
   
 }
--- Robot/test_joy_stick/test_joy_stick.ino
+++ Robot/test_joy_stick/test_joy_stick.ino
@@ -1,30 +1,25 @@
-/*quater
-  y+ 567 - 1023
-  x- 0 - 460
-  x+ 564 - 1023
-  y- 460 - 0
-*/
- 
-//Left motor
+#include "Arduino_LED_Matrix.h"
+#include "/home/keaw/github/IOT-Robot/Robot/animation/animation.h"
+
+ArduinoLEDMatrix matrix;
+
+// Left motor
 int ENA = 10;
 int IN1 = 9;
 int IN2 = 8;
-//Right motor
+
+// Right motor
 int ENB = 5;
 int IN3 = 7;
 int IN4 = 6;
 
-//servo
+// Servo
 int SV = 11;
 
-//joy stick
+// Joystick
 int b_stick = 4;
 int x_stick = A0;
 int y_stick = A1;
-//joy stick position
-int x_pos = 512;
-int y_pos = 512;
-bool b_button = 0;
 
 int speed_motorL = 0;
 int speed_motorR = 0;
@@ -32,204 +27,211 @@ int speed_motorR = 0;
 int B_L = 0;
 int B_R = 0;
 
+// Non-blocking animation state
+unsigned long last_frame_time = 0;
+int current_frame = 0;
+int current_anim_id = -1;
+
+void playAnimNonBlocking(const uint32_t anim[][4], int total_frames, int flip_mode, int anim_id) {
+  unsigned long now = millis();
+  if (current_anim_id != anim_id) {
+    current_anim_id = anim_id;
+    current_frame = 0;
+    last_frame_time = 0;
+  }
+  
+  if (now - last_frame_time >= 66) { // 66 ms frame delay
+    last_frame_time = now;
+    if (flip_mode == 0) loadFlippedYFrame(anim[current_frame]);
+    else if (flip_mode == 1) loadFlippedXFrame(anim[current_frame]);
+    else if (flip_mode == 2) loadFlippedXYFrame(anim[current_frame]);
+    current_frame = (current_frame + 1) % total_frames;
+  }
+}
+
 void setup() {
-  //analogWriteResolution(12);
-  // put your setup code here, to run once:
   Serial.begin(9600);
-//Left motor set up
-  pinMode(ENA,OUTPUT);
-  pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
-  //Right motor set up
-  pinMode(ENB,OUTPUT);
-  pinMode(IN3,OUTPUT);
-  pinMode(IN4,OUTPUT);
-  //Servo
-  pinMode(SV,OUTPUT);
-
-  //join stick
-  pinMode(b_stick,INPUT);
-  pinMode(x_stick,INPUT);
-  pinMode(y_stick,INPUT);
   
+  // Initialize LED matrix
+  matrix.begin();
+
+  // Left motor pins setup
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  // Right motor pins setup
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+
+  // Servo setup
+  pinMode(SV, OUTPUT);
+
+  // Joystick pins setup
+  pinMode(b_stick, INPUT_PULLUP);
+  pinMode(x_stick, INPUT);
+  pinMode(y_stick, INPUT);
 }
 
-void increse_speed(){
-  digitalWrite(IN1,0);
-  digitalWrite(IN2,1);
-  digitalWrite(IN3,0);
-  digitalWrite(IN4,1);
+void forward(int speed_motorL = 200, int speed_motorR = 200, int B_L = 0, int B_R = 15) {
+  digitalWrite(IN1, 0);
+  digitalWrite(IN2, 1);
 
-  for(int i = 2050;i<4096;i++){
-    analogWrite(ENA,i-B_L);
-    analogWrite(ENB,i-B_R);
+  digitalWrite(IN3, 0);
+  digitalWrite(IN4, 1);
+
+  analogWrite(ENA, speed_motorL - B_R);
+  analogWrite(ENB, speed_motorR - B_L);
+
+  int total_frames = sizeof(walk) / sizeof(walk[0]);
+  playAnimNonBlocking(walk, total_frames, 0, 1);
+}
+
+void backward(int speed_motorL = 200, int speed_motorR = 200, int B_L = 0, int B_R = 15) {
+  digitalWrite(IN1, 1);
+  digitalWrite(IN2, 0);
+
+  digitalWrite(IN3, 1);
+  digitalWrite(IN4, 0);
+
+  analogWrite(ENA, speed_motorL - B_R);
+  analogWrite(ENB, speed_motorR - B_L);
+
+  int total_frames = sizeof(walk) / sizeof(walk[0]);
+  playAnimNonBlocking(walk, total_frames, 1, 2);
+}
+
+void right_turn(int speed_motorR = 200, int speed_motorL = 0, int B_L = 0, int B_R = 0) {
+  digitalWrite(IN1, 0);
+  digitalWrite(IN2, 0);
+  digitalWrite(IN3, 0);
+  digitalWrite(IN4, 1);
+
+  analogWrite(ENA, speed_motorL - B_R);
+  analogWrite(ENB, speed_motorR - B_L);
+
+  int total_frames = sizeof(side_walk) / sizeof(side_walk[0]);
+  playAnimNonBlocking(side_walk, total_frames, 0, 3);
+}
+
+void left_turn(int speed_motorL = 200, int speed_motorR = 0, int B_L = 0, int B_R = 0) {
+  digitalWrite(IN1, 0);
+  digitalWrite(IN2, 1);
+  digitalWrite(IN3, 0);
+  digitalWrite(IN4, 0);
+
+  analogWrite(ENA, speed_motorL - B_R);
+  analogWrite(ENB, speed_motorR - B_L);
+
+  int total_frames = sizeof(side_walk) / sizeof(side_walk[0]);
+  playAnimNonBlocking(side_walk, total_frames, 2, 4);
+}
+
+void left_turnback(int speed_motorL = 200, int speed_motorR = 200, int B_L = 0, int B_R = 0) {
+  digitalWrite(IN1, 0);
+  digitalWrite(IN2, 1);
+  digitalWrite(IN3, 1);
+  digitalWrite(IN4, 0);
+
+  analogWrite(ENA, speed_motorL - B_R);
+  analogWrite(ENB, speed_motorR - B_L);
+
+  int total_frames = sizeof(radar) / sizeof(radar[0]);
+  playAnimNonBlocking(radar, total_frames, 0, 5);
+}
+
+void right_turnback(int speed_motorL = 200, int speed_motorR = 200, int B_L = 0, int B_R = 0) {
+  digitalWrite(IN1, 1);
+  digitalWrite(IN2, 0);
+  digitalWrite(IN3, 0);
+  digitalWrite(IN4, 1);
+
+  analogWrite(ENA, speed_motorL - B_R);
+  analogWrite(ENB, speed_motorR - B_R);
+
+  int total_frames = sizeof(radar) / sizeof(radar[0]);
+  playAnimNonBlocking(radar, total_frames, 2, 6);
+}
+
+void stop() {
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+
+  int total_frames = sizeof(heart) / sizeof(heart[0]);
+  playAnimNonBlocking(heart, total_frames, 0, 0);
+}
+
+void loadTransformedFrame(const uint32_t frame[4], bool flipX, bool flipY) {
+  uint32_t transformed[4] = {0, 0, 0, frame[3]};
+
+  for (int y = 0; y < 8; y++) {
+    for (int x = 0; x < 12; x++) {
+      int srcBit = y * 12 + x;
+      int srcWord = srcBit / 32;
+      int srcPos = 31 - (srcBit % 32);
+      bool isSet = (frame[srcWord] >> srcPos) & 1;
+
+      if (isSet) {
+        int new_x = flipX ? (11 - x) : x;
+        int new_y = flipY ? (7 - y) : y;
+
+        int dstBit = new_y * 12 + new_x;
+        int dstWord = dstBit / 32;
+        int dstPos = 31 - (dstBit % 32);
+        transformed[dstWord] |= (1UL << dstPos);
+      }
+    }
   }
-  for(int i = 4095;i>2051;i--){
-    analogWrite(ENA,i-B_L);
-    analogWrite(ENB,i-B_R);
-  }
+  matrix.loadFrame(transformed);
 }
 
-void  forward(int speed_motorL = 200,int speed_motorR = 200,int B_L = 0,int B_R = 0){
-  // Define parameter for L298N
- 
-  
-  digitalWrite(IN1,0);
-  digitalWrite(IN2,1);
-  
-  digitalWrite(IN3,0);
-  digitalWrite(IN4,1);
-
-
-  //left motor
-  analogWrite(ENA,speed_motorL-B_R);
-  //right motor
-  analogWrite(ENB,speed_motorR-B_L);
-  
+void loadFlippedXFrame(const uint32_t frame[4]) {
+  loadTransformedFrame(frame, true, false);
 }
 
-void backward(int speed_motorL = 200,int speed_motorR = 200,int B_L = 0,int B_R = 0){
-  
-  // Define parameter for L298N 
-  digitalWrite(IN1,1);
-  digitalWrite(IN2,0);
-  
-  digitalWrite(IN3,1);
-  digitalWrite(IN4,0);
-
-  //Define speed for motor
-  //left motor
-  analogWrite(ENA,speed_motorL-B_R);
-  //right motor
-  analogWrite(ENB,speed_motorR-B_L);
- 
+void loadFlippedYFrame(const uint32_t frame[4]) {
+  loadTransformedFrame(frame, false, true);
 }
 
-void right_turn(int speed_motorR = 200,int B_L = 0,int B_R = 0){
-  
-  
-  digitalWrite(IN3,0);
-  digitalWrite(IN4,1);
-
-  //Define speed for motor
-  
-  //right motor
-  analogWrite(ENB,speed_motorR-B_L);
-  
-
+void loadFlippedXYFrame(const uint32_t frame[4]) {
+  loadTransformedFrame(frame, true, true);
 }
 
-void left_turn(int speed_motorL = 200,int B_L = 0,int B_R = 0){
-  
-  // Define parameter for L298N 
-  
-  digitalWrite(IN1,0);
-  digitalWrite(IN2,1);
-
-  //Define speed for motor
-  
-  //right motor
-  analogWrite(ENA,speed_motorL-B_R );
-  
-
-}
-
-void left_turnback(int speed_motorL = 200,int speed_motorR = 200,int B_L = 0,int B_R = 0){
-  
-  // Define parameter for L298N 
-  digitalWrite(IN1,0);
-  digitalWrite(IN2,1);
-  digitalWrite(IN3,1);
-  digitalWrite(IN4,0);
-
-  //Define speed for motor
-  //left motor
-  analogWrite(ENA,speed_motorL-B_R);
-  //right motor
-  analogWrite(ENB,speed_motorR-B_L);
-  
-
-}
-void right_turnback(int speed_motorL = 200,int speed_motorR = 200,int B_L = 0,int B_R = 0){
-  // Define parameter for L298N 
-  digitalWrite(IN1,1);
-  digitalWrite(IN2,0);
-  digitalWrite(IN3,0);
-  digitalWrite(IN4,1);
-
-  //Define speed for motor
-  //left motor
-  analogWrite(ENA,speed_motorL-B_R);
-  //right motor
-  analogWrite(ENB,speed_motorR-B_R);
-  
-}
-void stop(){
-  analogWrite(ENA,0);
-  
-  analogWrite(ENB,0);
-  delay(1000);
-}
-
-void  servo(int degree){
-  //digitalWrite(SV, 1);
-  //Servo(SV,degree);
-}
-/*quater
-  y+ 567 - 1023
-  x- 0 - 460
-  x+ 564 - 1023
-  y- 460 - 0
-*/
-
-void joy_stick(){
+void joy_stick() {
   int x_pos = analogRead(x_stick);
   int y_pos = analogRead(y_stick);
-  b_button = analogRead(b_stick);
-    if(((y_pos >= 629)&&(y_pos <=1023))&&b_button == 1){
-      speed_motorL = map(y_pos,629,1023,0,255);
-      speed_motorR = map(y_pos,629,1023,0,255);
-      Serial.println("Forward");
-      forward(speed_motorL,speed_motorR);
-    }
-    else if(((y_pos  >= 0)&&(y_pos <=460))&&b_button == 1){
-      y_pos = (y_pos -460)*(-1);
-      speed_motorL = map(y_pos,0,460,0,255);
-      speed_motorR = map(y_pos,0,460,0,255);
-      Serial.println("backward");
-      backward();
-    }
-    else if(((x_pos >= 0)&&(x_pos<=460))&&b_button == 1){
-      Serial.println("right turn");
-      right_turn();
-    }
-    else if(((x_pos >= 629)&&(x_pos<=1023))&&b_button == 1){
-      Serial.println("left turn");
-      left_turn();
-    }
-    
-    else{
-      Serial.println("stop");
-     stop(); 
-     }
-    
+
+  if (y_pos >= 629 && y_pos <= 1023) {
+    speed_motorL = map(y_pos, 629, 1023, 100, 255);
+    speed_motorR = map(y_pos, 629, 1023, 100, 255);
+    Serial.println("Forward");
+    forward(speed_motorL, speed_motorR);
+  }
+  else if (y_pos >= 0 && y_pos <= 460) {
+    int inv_y = (460 - y_pos);
+    speed_motorL = map(inv_y, 0, 460, 100, 255);
+    speed_motorR = map(inv_y, 0, 460, 100, 255);
+    Serial.println("Backward");
+    backward(speed_motorL, speed_motorR);
+  }
+  else if (x_pos >= 0 && x_pos <= 460) {
+    int inv_x = (460 - x_pos);
+    speed_motorR = map(inv_x, 0, 460, 100, 255);
+    Serial.println("Right turn");
+    right_turn(speed_motorR);
+  }
+  else if (x_pos >= 629 && x_pos <= 1023) {
+    speed_motorL = map(x_pos, 629, 1023, 100, 255);
+    Serial.println("Left turn");
+    left_turn(speed_motorL);
+  }
+  else {
+    Serial.println("Stop");
+    stop();
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int d_time = 1000; 
-  Serial.print("x :");
-  Serial.println(analogRead(x_stick));
-  Serial.print("y :");
-  Serial.println(analogRead(y_stick));
-  
-  
   joy_stick();
-    
-
-  
-  /*
-  forward(1000,1000);
-  delay(1000);
-  */
 }
