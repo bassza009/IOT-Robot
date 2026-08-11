fb4c7e7c772697f42aabdbe1e66369e5840828a3
bassza009 <bsspawat@gmail.com>
2026-08-12 02:12:17 +0700

    Fix ex.exam1 IoT

--- IOT/mid_term_exam_1term/exam1/exam1.ino
+++ IOT/mid_term_exam_1term/exam1/exam1.ino
@@ -1,22 +1,27 @@
  const int ledPin[4] = {9,10,11,12};
-
+ const int button[2] = {2,3};
+ int i=0;
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT); 
+  for(int i= 0;i<2;i++){
+    pinMode(button[i], INPUT);
+  } 
   for(int i=0;i<4;i++){
     pinMode(ledPin[i], OUTPUT);
   }
 }
 
 void loop() {
-  int button;
+  int button1;
+  int button2;
   int decimal;
-  int i=0;
   
-  button = digitalRead(2);
-  while(button ==0 &&i<=15){
+  
+  button1 = digitalRead(button[0]);
+  button2 = digitalRead(button[1]);
+  while(button1 == 0 &&i<=15){
       decimal = i;
-      button = digitalRead(2);
+      button1 = digitalRead(button[0]);
       
       Serial.print("I : ");
       Serial.println(i);
@@ -28,18 +33,39 @@ void loop() {
 
       i++;
       delay(1000);
-    while(i >15 && button ==0 ){
+    while(i >15 && button1 ==0 ){
       
-      button = digitalRead(2);
+      button1 = digitalRead(button[0]);
       Serial.println("I no longer than 15");
       
       delay(100);
       
     }
-    
+     
   }
-  deadled(button);
+  while(button2 == 0 && i >=0){
+      decimal = i;
+      button2 = digitalRead(button[1]);
+      
+      Serial.print("I : ");
+      Serial.println(i);
 
+      led_blink_binary(decimal >> 0 & 1,ledPin[0]); // >> mean point to array of binary 
+      led_blink_binary(decimal >> 1 & 1,ledPin[1]);
+      led_blink_binary(decimal >> 2 & 1,ledPin[2]);
+      led_blink_binary(decimal >> 3 & 1,ledPin[3]);
+
+      i--;
+      delay(1000);  
+      while(i <= 0 && button2 ==0 ){
+        
+        button2 = digitalRead(button[1]);
+        Serial.println("I no less than 0");
+        
+        delay(100);
+      
+      }
+  }
 }
 
 void led_blink_binary(int n,int pin){
@@ -49,6 +75,7 @@ void led_blink_binary(int n,int pin){
     digitalWrite(pin, LOW);
   }
 }
+
 
 void deadled(int button){
   if(button == 1){
